fc8a9b5a6a27f2c66f363eec6f8857267bdeb5c7
Pierre Berchtold <pierre.berchtold@gmail.com>
2018-03-27 17:15:32 +0200

    Remove Unneeded Stuff

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/Pierre/Dev/gatsby-cockpit-portfolio/src/layouts/index.js"
-  import data from "/Users/Pierre/Dev/gatsby-cockpit-portfolio/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
