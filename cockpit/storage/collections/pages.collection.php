1138f57cfcff7d8d76f7fdbd0c2234893d37e7a0
Pierre Berchtold <pierre.berchtold@gmail.com>
2018-03-27 17:29:40 +0200

    Add Cockpit Storage to Repository

---
+++ cockpit/storage/collections/pages.collection.php
@@ -0,0 +1,86 @@
+<?php
+ return array (
+  'name' => 'pages',
+  'label' => 'Pages',
+  '_id' => 'pages5ab119e759f12',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'title',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'slug',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'content',
+      'label' => '',
+      'type' => 'wysiwyg',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => true,
+  'in_menu' => false,
+  '_created' => 1521555943,
+  '_modified' => 1521556179,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+);
---
+++ cockpit/storage/collections/projects.collection.php
@@ -0,0 +1,104 @@
+<?php
+ return array (
+  'name' => 'projects',
+  'label' => 'Projects',
+  '_id' => 'projects5ab119ccb6851',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'title',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'slug',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'preview',
+      'label' => '',
+      'type' => 'image',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'content',
+      'label' => '',
+      'type' => 'wysiwyg',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => true,
+  'in_menu' => false,
+  '_created' => 1521555916,
+  '_modified' => 1521557704,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+    ),
+    'read' => 
+    array (
+    ),
+    'update' => 
+    array (
+    ),
+    'delete' => 
+    array (
+    ),
+  ),
+);
